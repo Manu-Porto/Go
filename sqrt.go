1cab542af1aa2eb6584b68030f62f545315adb2c
Manu Porto <62604663+Manu-Porto@users.noreply.github.com>
2023-12-29 22:12:50 -0300

    sqrt.go

--- sqrt.go
+++ sqrt.go
@@ -10,10 +10,10 @@ func sqrt(x float64) string {
 	if x < 0 { 
 		return sqrt(-x) + "i"		// for complex numbers
     } 
-  	var z float64 = 1				  // here you can change the "z" number that goes on in the first interaction 
+  	var z float64 = 1			// here you can change the "z" number that goes on in the first interaction 
   	for i:=0; i< 5; i++ {			// you may change "i" numbers of interaction also
 	  	z -= (z * z - x) / (2 * z)}
-  return fmt.Sprint(z)        // still don't know why to use this, but i know i need to
+  return fmt.Sprint(z)       			// still don't know why to use this, but i know i need to
 }
 
 
